66104627482d356589ea43d68353a335cb616742
Nikita <voitko1994@gmail.com>
2024-03-31 14:51:59 +0200

    Welcome User implemented V2

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,8 +2,9 @@
 def main():
   print("Welcome to the Brain Games!")
 
+  #import welcome user from cli
+  from brain_games.cli import welcome_user
+  welcome_user()
+  
 if __name__ == '__main__':
   main()
-
-#import welcome user from cli
-from brain_games.cli import welcome_user
