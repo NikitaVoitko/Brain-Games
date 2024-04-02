ef8e55765d02e3e94fc9fa88b912202a01b48d45
Nikita <voitko1994@gmail.com>
2024-04-02 16:40:21 +0200

    Litner + Refactoring Engine

--- brain_games/engine/engine.py
+++ brain_games/engine/engine.py
@@ -12,15 +12,10 @@ def game_engine(game_func):
             print("Correct!")
             question_count += 1
         else:
-            if isinstance(correct_answer, int):
-                print(f"'{user_answer}' is an incorrect answer, {name}!"
-                      f"The correct answer was '{correct_answer}'."
-                      f"  Let's try again.")
-                incorrect_answer = True
-            else:
-                print(f"'{user_answer}' is an incorrect answer, {name}!"
-                      f" Let's try again.")
-                incorrect_answer = True
+            print(f"'{user_answer}' is an incorrect answer, {name}! "
+                  f"The correct answer was '{correct_answer}'."
+                  f" Let's try again.")
+            incorrect_answer = True
             break
     if not incorrect_answer:
         print(f"Congratulations, {name}!")
