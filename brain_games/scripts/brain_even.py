96769adcb217bb027f88ccd4b0d5c35fbce6f051
Nikita <voitko1994@gmail.com>
2024-04-02 21:19:16 +0200

    CodeClimate Fix Ver.2

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,9 +2,8 @@
 def main():
 
     # import game1 and engine
-    from brain_games.games.game1 import is_even_game
     from brain_games.engine.game_runner import game_runner
-    game_runner(is_even_game)
+    game_runner("is_even_game")
 
 
 if __name__ == '__main__':
