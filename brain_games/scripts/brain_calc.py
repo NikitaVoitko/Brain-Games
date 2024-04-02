6cb90bb75532cac8ce9fc93562ce39de9f500fbe
Nikita <voitko1994@gmail.com>
2024-04-02 21:18:28 +0200

    CodeClimate Fix Ver.2

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -2,9 +2,9 @@
 def main():
 
     # import game1 and engine
-    from brain_games.games.game2 import calculator_game
+
     from brain_games.engine.game_runner import game_runner
-    game_runner(calculator_game)
+    game_runner("calculator_game")
 
 
 if __name__ == '__main__':
