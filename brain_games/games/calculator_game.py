32f3babfd73cad359b1c74e2ef0e515aca5df32c
Philip Creutzmann <87417551+pcreutzmann@users.noreply.github.com>
2024-04-08 15:11:36 +0200

    Update calculator_game.py

--- brain_games/games/calculator_game.py
+++ brain_games/games/calculator_game.py
@@ -3,7 +3,7 @@ import random
 
 
 def calculator_game():
-
+    # to-do: docstring missing
     print('What is the result of the expression?')
     number1 = random.randint(50, 100)
     number2 = random.randint(1, 50)
@@ -13,6 +13,8 @@ def calculator_game():
     question = f"Question: {number1} {random_operator} {number2}"
     print(question)
     answer = prompt.integer()
+    # to-do: error handling if no proper number provided
+    # e.g. print(f"Provided input \"{answer}\" is not an number. Please provide a new answer.")
 
     if random_operator == '+':
         correct_answer = number1 + number2
@@ -20,5 +22,5 @@ def calculator_game():
         correct_answer = number1 - number2
     else:
         correct_answer = number1 * number2
-
+    # to-do define readable variable before returning
     return answer == correct_answer, correct_answer, answer
